c8adce529e3cead5138a895a76ca6e547f538981
Jarrod Flesch <jarrodmflesch@gmail.com>
2022-12-06 12:50:59 -0500

    fix: only allow logged in users to retrieve the version

--- src/endpoints/readPayloadVersion.ts
+++ src/endpoints/readPayloadVersion.ts
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 import { PayloadHandler } from 'payload/config';
+import { Forbidden } from 'payload/errors';
 
 export const readPayloadVersion: PayloadHandler = (req, res, next) => {
+  if (!req.user) throw new Forbidden
+
   try {
     const data: any = fs.readFileSync(path.resolve(__dirname, '../../package.json'));
     const packageJson = JSON.parse(data);
