1406579d9f1c19beefc87ad05eb29c69c772d74e
Andres <andres@netzstrategen.com>
2023-08-03 21:32:07 +0200

    fix: clear link field relationship when link.type is custom (#25)

--- src/fields/link.ts
+++ src/fields/link.ts
@@ -66,6 +66,11 @@ const link: LinkType = ({
         admin: {
           condition: (_, siblingData) => siblingData?.type === 'reference',
         },
+        hooks: {
+          beforeChange: [
+            ({ siblingData, value }) => (siblingData?.type !== "reference" ? null : value),
+          ],
+        },
       },
       {
         name: 'url',
