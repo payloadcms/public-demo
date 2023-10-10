1687b512c996a0e580d21a0f7c4b2fc63a5c1a98
Alessio Gravili <alessio@gravili.de>
2023-10-10 19:02:23 +0200

    chore: adjust eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,7 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
 }
