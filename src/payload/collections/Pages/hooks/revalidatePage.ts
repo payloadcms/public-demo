9dd96a5e48f676bd30a19a5a45b8abdba0b63161
Jessica Chowdhury <jessica@trbl.design>
2023-11-21 11:28:36 +0000

    chore: update payload and catch revalidation errors

--- src/payload/collections/Pages/hooks/revalidatePage.ts
+++ src/payload/collections/Pages/hooks/revalidatePage.ts
@@ -6,9 +6,14 @@ import { revalidate } from '../../../utilities/revalidate'
 // Notice that the hook itself is not async and we are not awaiting `revalidate`
 // Only revalidate existing docs that are published
 // Don't scope to `operation` in order to purge static demo pages
-export const revalidatePage: AfterChangeHook = ({ doc, req: { payload } }) => {
+export const revalidatePage: AfterChangeHook = async ({ doc, req: { payload } }) => {
+  console.log(doc)
   if (doc._status === 'published') {
-    void revalidate({ collection: 'pages', payload, slug: doc.slug })
+    try {
+      await revalidate({ collection: 'pages', payload, slug: doc.slug })
+    } catch (error) {
+      // do not output error on build
+    }
   }
 
   return doc
