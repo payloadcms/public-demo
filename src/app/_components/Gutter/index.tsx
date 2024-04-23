361fc097f073ef9346287882fb50936d9e4b12be
Patrik Kozak <patrik@payloadcms.com>
2024-04-23 13:53:13 -0400

    fix: build fixes

--- src/app/_components/Gutter/index.tsx
+++ src/app/_components/Gutter/index.tsx
@@ -1,5 +1,3 @@
-import type { Ref } from 'react'
-
 import React, { forwardRef } from 'react'
 
 import classes from './index.module.scss'
@@ -8,11 +6,10 @@ type Props = {
   children: React.ReactNode
   className?: string
   left?: boolean
-  ref?: Ref<HTMLDivElement>
   right?: boolean
 }
 
-export const Gutter: React.FC<Props> = forwardRef<HTMLDivElement, Props>((props, ref) => {
+export const Gutter = forwardRef<HTMLDivElement, Props>((props, ref) => {
   const { children, className, left = true, right = true } = props
 
   return (
